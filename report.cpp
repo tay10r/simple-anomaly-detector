501e4fd948b0ead9c6b01f460c73c9f663839286
Taylor Holberton <taylor.holberton@proton.me>
2024-11-14 16:16:26 -0500

    Fix report

--- report.cpp
+++ report.cpp
@@ -27,11 +27,15 @@ report(const double timestamp, const double anomaly, const char* ip, const int u
     return false;
   }
 
+  sockaddr_in bind_address{};
+  uv_ip4_addr("0.0.0.0", 0, &bind_address);
+
   uv_loop_t loop{};
   uv_loop_init(&loop);
 
   uv_udp_t socket{};
   uv_udp_init(&loop, &socket);
+  uv_udp_bind(&socket, reinterpret_cast<const sockaddr*>(&bind_address), UV_UDP_REUSEADDR);
   uv_udp_set_broadcast(&socket, 1);
 
   uv_udp_send_t write_op;
@@ -42,8 +46,16 @@ report(const double timestamp, const double anomaly, const char* ip, const int u
   send_buffer.base = reinterpret_cast<char*>(&data[0]);
   send_buffer.len = sizeof(data);
 
+  auto on_write = [](uv_udp_send_t*, const int status) {
+    if (status == 0) {
+      SPDLOG_INFO("Sent anomaly report.");
+    } else {
+      SPDLOG_ERROR("Failed to send anomaly report: {}", uv_strerror(status));
+    }
+  };
+
   const auto send_err =
-    uv_udp_send(&write_op, &socket, &send_buffer, 1, reinterpret_cast<const sockaddr*>(&address), nullptr);
+    uv_udp_send(&write_op, &socket, &send_buffer, 1, reinterpret_cast<const sockaddr*>(&address), on_write);
   if (send_err != 0) {
     SPDLOG_ERROR("Failed to send UDP message: {}", uv_strerror(send_err));
   }
